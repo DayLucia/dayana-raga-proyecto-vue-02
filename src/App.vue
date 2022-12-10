794f6a0120d8c052396f87fefd43c3228490c55d
DayRaga <107507675+DayLucia@users.noreply.github.com>
2022-12-10 17:12:23 -0300

    Feat: Limpieza, cambio de Nav e inserción de la primera img

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,17 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="green"
       dark
     >
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
+          :src="require('./assets/logo.gif')"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          width="80"
         />
       </div>
 
@@ -32,8 +23,14 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Home</span>
+      </v-btn>
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Recetas</span>
       </v-btn>
     </v-app-bar>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{
+        themes:{
+            light:{
+                red: '#8C0303',
+                green: '#AFCC0F',
+                yellow: '#F2B705',
+            }
+        }
+    }
 });
