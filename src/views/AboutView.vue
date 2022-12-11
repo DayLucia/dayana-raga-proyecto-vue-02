7b53200c258aab8d6128b9c2b01480a89b9e188b
DayRaga <107507675+DayLucia@users.noreply.github.com>
2022-12-11 00:08:16 -0300

    fEAT: Cambios

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <HelloAbout/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloAbout from '@/components/HelloAbout.vue'
+export default {
+  name: 'about-view',
+  data: ()=>({
+    
+  })
+}
+</script>
