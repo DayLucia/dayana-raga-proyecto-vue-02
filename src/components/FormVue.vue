c76151ac0c426981d1adc3ca868c21830a9c7046
DayRaga <107507675+DayLucia@users.noreply.github.com>
2022-12-10 21:36:52 -0300

    Feat: Formulario y validacion

--- src/components/FormVue.vue
+++ src/components/FormVue.vue
@@ -7,26 +7,32 @@
             </v-col>
         </v-row>
         <div>
-        <v-alert
-          :value="alert"
-          color="error"
-          dark
-          icon="mdi-home"
-          transition="scale-transition"
-        >
-          Phasellus tempus. Fusce ac felis sit amet ligula pharetra condimentum. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Pellentesque posuere. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo.
-    
-          Phasellus nec sem in justo pellentesque facilisis. Phasellus magna. Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo. In hac habitasse platea dictumst. Praesent turpis.
-        </v-alert>
-        <v-form v-on:submit.prevent="checkForm" method="get">
-            <v-row>
+            <v-alert
+                :value="alert"
+                color="error"
+                dark
+                icon="mdi-home"
+                transition="scale-transition"
+                >
+                {{mensajeAlert}}
+            </v-alert>
+            <v-alert
+                :value="success"
+                color="success"
+                dark
+                icon="mdi-home"
+                transition="scale-transition"
+                >
+                La receta se publicó correctamente.
+            </v-alert>
+            <v-form v-on:submit.prevent="checkForm" method="get">
+                <v-row>
                 <v-col cols="6">
                     <v-text-field v-model="autor" label="Autor del plato" placeholder="Autor"></v-text-field>
                     <v-text-field v-model="nReceta" label="Nombre del plato" placeholder="Nombre de la receta"></v-text-field>
                     <v-row>
                         <v-col cols="6">
                             <v-select
-                                :tipos="items"
                                 label="Tipo de plato"
                             ></v-select>
                         </v-col>
@@ -40,94 +46,157 @@
                     <v-textarea v-model="preparacion" rows="4" label="Preparación" placeholder="Separar los parrafos con un salto de linea"></v-textarea>
                 </v-col>
                 <v-col cols="12" class="text-center">
-                    <v-btn color="green primary" v-on:click="!alert">Publicar</v-btn>
+                    <v-btn color="green primary" v-on:click.prevent="validateForm">Publicar</v-btn>
                 </v-col>
             </v-row>
         </v-form>
     </div>
-    </v-container>
+        <v-row class="align-center">
+            <v-col cols="6">
+        <v-img
+        :src="require('../assets/platoTerciario.png')"
+        class="my-6"
+        contain 
+        />
+      </v-col>       
+      <v-col cols="6">
+          <v-row>
+              <v-col cols="12">
+                  <h2>
+                      {{publicadoTitle}}
+                    </h2>
+                </v-col>
+                <v-col cols="12">
+                    <p class="publicadoText">
+                        {{publicadoText}}
+                    </p>
+                    <v-btn
+                    color="red"
+                    raised
+                    class="error"
+                    >Mis recetas</v-btn>
+                </v-col>
+            </v-row>
+        </v-col>       
+    </v-row>
+</v-container>
 
 </template>
 <script>
 export default {
     name: 'FormVue',
     data: () => ({
-        alert: false,
         autor: null,
         nReceta: null,
         tiempo: null,
         tipo: 'Almuerzo',
         ingredientes: null,
         preparacion: null,
-        // contador : 0, //Contador para el id de la receta
+        contador : 0, //Contador para el id de la receta
         // duracionExtraida: [], //Aqui se guardan los tiempos extraidos de la cadena de texto
-        // ingredientesExtraidos: [], //separados con ,
-        // preparacionExtraida: [], //separados con saltos de linea
+        ingredientesExtraidos: [], //separados con ,
+        preparacionExtraida: [], //separados con saltos de linea
         tipos: ['Desayuno', 'Almuerzo', 'Cena', 'Merienda'],
         // nombreNews: null,
         // emailNews: null,
+        alert: false,
+        mensajeAlert: '',
+        success: false,
+        publicadoTitle: '¿Ya publicaste tu receta?',
+        publicadoText: 'Mirala ahora en',
+
     }),
-  
-  /*
-  
-    `*/
 
-//   methods: {
+  methods: {
+    validateForm: function(e){
+        this.alert = false;
+        this.success = false;
+        this.mensajeAlert= '';
+        console.log(e);
+        if (!this.autor) {
+            this.mensajeAlert += 'El nombre del autor es obligatorio, ';
+            this.alert = true;
+        }
+        if (!this.nReceta) {
+            this.mensajeAlert += 'El nombre del plato es obligatorio, ';
+            this.alert = true;
+        }
+        if (!this.tiempo) {
+            this.mensajeAlert += 'El tiempo de preparación es obligatorio, ';
+            this.alert = true;
+        }
+        if (!this.ingredientes) {
+            this.mensajeAlert += 'Los ingredientes son obligatorios, ';
+            this.alert = true;
+        }
+        if (!this.preparacion) {
+            this.mensajeAlert += 'La preparación es obligatoria.';
+            this.alert = true;
+        }
+        if(!this.mensajeAlert){
+            this.success = true;
+            this.checkForm()
+        }
+        setTimeout(()=>{
+            this.success = false;
+            this.alert = false;
+        }, 2000);
+    },
 //     //Recibimos y verificamos los datos
-//     checkForm: function() {
-//       if (this.ingredientes !== null) {
-//         arrayIngredientes = this.ingredientes.split(",");
-//           //For para guardar los ingredientes en un array        
-//         for (var i = 0; i < arrayIngredientes.length; i++) {
-//           this.ingredientesExtraidos.push(arrayIngredientes[i]);
-//         } //Fin del for
+    checkForm: function() {
+      if (this.ingredientes !== null) {
+        let arrayIngredientes = this.ingredientes.split(",");
+          //For para guardar los ingredientes en un array        
+        for (var i = 0; i < arrayIngredientes.length; i++) {
+          this.ingredientesExtraidos.push(arrayIngredientes[i]);
+        } //Fin del for
         
-//         arrayPreparacion = this.preparacion.split("\\n");
-//         //For para separar la preparacion en un array
-//         for (var i = 0; i < arrayPreparacion.length; i++) {
-//           this.preparacionExtraida.push(arrayPreparacion[i]);
-//         } //Fin del for
+        let arrayPreparacion = this.preparacion.split("\\n");
+        //For para separar la preparacion en un array
+        for (var item = 0; item < arrayPreparacion.length; item++) {
+          this.preparacionExtraida.push(arrayPreparacion[item]);
+        } //Fin del for
         
-//         this.contador++;
-//         //Añadimos la receta
-//         this.armarReceta(this.autor, this.nReceta, this.tiempo, this.tipo, this.ingredientesExtraidos, this.preparacionExtraida, this.contador)
+        let contador2 = this.contador++;
+        //Añadimos la receta
+        this.armarReceta(this.autor, this.nReceta, this.tiempo, this.tipo, this.ingredientesExtraidos, this.preparacionExtraida, this.contador)
   
-//         this.autor = null;
-//         this.nReceta = null;
-//         this.tiempo = null;
-//         this.tipo = null;
-//         this.ingredientes = null;
-//         this.preparacion = null;
-//         this.contador = this.contador 
-//         this.ingredientesExtraidos = [];
-//         this.preparacionExtraida = [];
-//         // this.duracionExtraida = [];
-//       }
-//     },
-//     //Metodo de armar los datos del array recetas
-//     armarReceta: function(nombre, receta, duracion, tipoPlato, ingredientes, preparacion, id) {
-//       info = localStorage.getItem('recetas');
-//       if (info == null) {
-//         info = [];
-//       } else {
-//         info = JSON.parse(info);
-//       }
-//       info.push({
-//         nombre: receta,
-//         autor: nombre,
-//         duracion: duracion,
-//         tipoPlato: tipoPlato,
-//         ingredientes: ingredientes,
-//         preparacion: preparacion,
-//         id : id,
-//       })
-//       localStorage.setItem('recetas', JSON.stringify(info))
-//     },
-//     //Metodo para resetear newsletter
-//     resetNews: function() {
-//       this.nombreNews = null;
-//       this.emailNews = null;
+        this.autor = null;
+        this.nReceta = null;
+        this.tiempo = null;
+        // this.tipo = null;
+        this.ingredientes = null;
+        this.preparacion = null;
+        this.contador = contador2 ;
+        this.ingredientesExtraidos = [];
+        this.preparacionExtraida = [];
+        // this.duracionExtraida = [];
+      }
+    },
+    //Metodo de armar los datos del array recetas
+    armarReceta: function(nombre, receta, duracion, tipoPlato, ingredientes, preparacion, id) {
+      let info = localStorage.getItem('recetas');
+      if (info == null) {
+        info = [];
+      } else {
+        info = JSON.parse(info);
+      }
+      info.push({
+        nombre: receta,
+        autor: nombre,
+        duracion: duracion,
+        tipoPlato: tipoPlato,
+        ingredientes: ingredientes,
+        preparacion: preparacion,
+        id : id,
+      })
+      localStorage.setItem('recetas', JSON.stringify(info))
+    },
+    //Metodo para resetear newsletter
+    resetNews: function() {
+      this.nombreNews = null;
+      this.emailNews = null;
 
-//     },
-}
+    },
+}}
 </script>
